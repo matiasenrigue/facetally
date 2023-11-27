1105fdaa3b4d7d91260b81a6629c5930d69d9713
Marc Klingenberg <klingenberg.marc20@gmail.com>
2023-11-27 18:04:10 +0100

    ml_logic init

---
+++ face_tally/interface/main.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+from colorama import Fore, Style
+
+
+def train():
+    """
+    - Download processed data from TO BE DETERMINED
+    - Train on the preprocessed dataset
+    - Store training results and model weights
+
+    Return loss as a float
+    """
+
+    print(Fore.MAGENTA + "\nUse case: train" + Style.RESET_ALL)
+    print(Fore.BLUE + "\nLoading preprocessed validation data..." + Style.RESET_ALL)
+
+    # Train model using `model.py`
+    # model = load_model()
