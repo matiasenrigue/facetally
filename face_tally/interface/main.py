3a2589bca7b5209b4cdd3166418a501b290fc467
Marc Klingenberg <klingenberg.marc20@gmail.com>
2023-11-28 10:49:46 +0100

    basic structure implemented

--- face_tally/interface/main.py
+++ face_tally/interface/main.py
@@ -4,17 +4,47 @@ import pandas as pd
 from colorama import Fore, Style
 
 
+def preprocess():
+    """
+    - Query the raw dataset from TO BE DETERMINED
+    - Cache query result as a local CSV if it doesn't exist locally
+    - Process query data
+    - Store processed data on your personal BQ (truncate existing table if it exists)
+    - No need to cache processed data as CSV (it will be cached when queried back from BQ during training)
+    """
+    pass
+
+
 def train():
     """
-    - Download processed data from TO BE DETERMINED
+    - Retrieve data from BigQuery, or from `cache_path` if the file exists
+    - Store at `cache_path` if retrieved from BigQuery for future use
     - Train on the preprocessed dataset
     - Store training results and model weights
 
     Return loss as a float
     """
+    pass
 
-    print(Fore.MAGENTA + "\nUse case: train" + Style.RESET_ALL)
-    print(Fore.BLUE + "\nLoading preprocessed validation data..." + Style.RESET_ALL)
 
-    # Train model using `model.py`
-    # model = load_model()
+def evaluate():
+    """
+    Evaluate the performance of the latest production model on processed data
+    Return loss as a float
+    """
+    pass
+
+
+def pred():
+    """
+    Make a prediction using the latest trained model
+    """
+    pass
+
+
+if __name__ == "__main__":
+    # preprocess()
+    # train()
+    # evaluate()
+    # pred()
+    pass
---
+++ face_tally/ml_logic/data.py
@@ -0,0 +1,2 @@
+import pandas as pd
+from face_tally.params import *
