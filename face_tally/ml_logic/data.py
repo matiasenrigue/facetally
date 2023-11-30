303954185f1e1f4e5b4ad92554c9785f2981bee0
Marc Klingenberg <klingenberg.marc20@gmail.com>
2023-11-30 12:58:07 +0100

    GCP logic added

--- face_tally/ml_logic/data.py
+++ face_tally/ml_logic/data.py
@@ -3,6 +3,7 @@ from face_tally.params import *
 import os
 import pandas as pd
 import tensorflow as tf
+from google.cloud import storage
 
 
 def load_annotations_csv():
@@ -25,3 +26,38 @@ def load_image(image_path: str) -> tf.Variable:
     image = tf.io.read_file(image_path)
     image = tf.image.decode_jpeg(image, channels=3)
     return image
+
+
+def download_images_from_GCP(
+    bucket_name: str, folder_path: str, destination_folder: str, overwrite=False
+) -> bool:
+    """
+    Given a BUCKET_NAME, it updated the data in bucket folder path to a local destination folder
+    If overwrite True, the data from the bucket overwrites the local data
+    Returns True if something is downloaded or updated
+    """
+    # Initialize the Cloud Storage client
+    storage_client = storage.Client()
+
+    # Get the bucket
+    bucket = storage_client.bucket(bucket_name)
+
+    # List objects in the specified folder path
+    folder_objects = bucket.list_blobs(prefix=folder_path)
+
+    # Create a local folder if it doesn't exist
+    os.makedirs(destination_folder, exist_ok=True)
+
+    # Download each image file from the folder
+    count = 0
+    for obj in folder_objects:
+        # Construct local file path for downloading
+        local_file_path = os.path.join(destination_folder, os.path.basename(obj.name))
+        # Check if the file exists locally
+        if overwrite or not os.path.exists(local_file_path):
+            # Download the object to the local file
+            obj.download_to_filename(local_file_path)
+            count += 1
+            print(f"Downloaded '{obj.name}' to '{local_file_path}'.  Count: {count}")
+
+    return True if count > 0 else False
