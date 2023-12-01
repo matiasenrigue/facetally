9d7dca75ee99a25a80ea8b117a62aeadfe4214bf
Marc Klingenberg <klingenberg.marc20@gmail.com>
2023-12-01 14:38:41 +0100

    BACKBONE_SIZE

--- face_tally/params.py
+++ face_tally/params.py
@@ -5,6 +5,7 @@ SPLIT_RATIO = float(os.environ.get("SPLIT_RATIO"))
 BATCH_SIZE = int(os.environ.get("BATCH_SIZE"))
 LEARNING_RATE = float(os.environ.get("LEARNING_RATE"))
 EPOCH = int(os.environ.get("EPOCH"))
+BACKBONE_SIZE = os.environ.get("BACKBONE_SIZE")
 
 GLOBAL_CLIPNORM = float(os.environ.get("GLOBAL_CLIPNORM"))
 
