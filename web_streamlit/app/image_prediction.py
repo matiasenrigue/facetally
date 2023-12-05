608f1dee6fd18807addecf5520ba1e7ab61b6fd3
Matias Enrigue <matienrigue1@gmail.com>
2023-12-05 11:52:58 +0100

    changed aspect bbox

--- web_streamlit/app/image_prediction.py
+++ web_streamlit/app/image_prediction.py
@@ -1,4 +1,6 @@
+from ultralytics import YOLO
 import numpy as np
+import datetime
 from PIL import Image
 from pillow_heif import register_heif_opener
 import cv2
@@ -11,6 +13,42 @@ This script has functions to be able to predict where object are in a picture:
 - save_image will save the image when the code is runned localy
 - full_process will run the 3 functions
 """
+
+
+def predict_bounding_boxes(image, model) -> dict:
+    """
+    Gets the bounding boxes that say where objects are located in an images, returns:
+    - class of the object
+    - position in the image
+    - confidence of that object being it
+
+    Does that by using our models prediction
+    """
+
+    results = model.predict(image)
+
+    result = results[0]
+    box = result.boxes[0]
+
+    bound_boxes = []
+
+    for box in result.boxes:
+        cordenadas_xywh = box.xyxy[0].tolist()
+
+        predicted_class = box.cls[0]
+        class_name = result.names[predicted_class.item()]
+
+        confidence = round(box.conf[0].item(), 2)
+
+        dict = {
+            "Object type": class_name,
+            "Coordinates": cordenadas_xywh,
+            "Probability": confidence,
+        }
+
+        bound_boxes.append(dict)
+
+    return bound_boxes
 
 
 def create_image(original_image_array: np.array, bound_boxes: dict) -> np.array:
@@ -32,8 +70,8 @@ def create_image(original_image_array: np.array, bound_boxes: dict) -> np.array:
         probability = box_info["Probability"]
 
         coordinates = box_info["Coordinates"]
-        color = (135, 206, 250)  # Color for the bounding box
-        thickness = 5
+        color = (90, 255, 127)  # Color for the bounding box
+        thickness = 2
 
         # Convert float coordinates to integers
         coordinates = [int(coord) for coord in coordinates]
@@ -48,15 +86,24 @@ def create_image(original_image_array: np.array, bound_boxes: dict) -> np.array:
         )
 
         # Annotate with object type and probability
-        label = f"{object_type} ({probability:.2f})"
+        label = f"{object_type}"
+
+        cv2.rectangle(
+            opencv_image,
+            (coordinates[0], coordinates[1] - 5),
+            (coordinates[0] + 80, coordinates[1] - 25),
+            (76, 166, 96),
+            -1,  # thickness = -1 to fill the entire thing
+        )
+
         cv2.putText(
             opencv_image,
             label,
             (coordinates[0], coordinates[1] - 10),
             cv2.FONT_HERSHEY_SIMPLEX,
-            1.5,
+            0.75,
             (255, 255, 255),
-            4,
+            2,
         )
         # cv2.FONT_HERSHEY_SIMPLEX, size, color, width
 
@@ -65,3 +112,47 @@ def create_image(original_image_array: np.array, bound_boxes: dict) -> np.array:
 
     # Display or save the annotated image as needed
     return annotated_image
+
+
+def save_image(image_created, file_save_name=None) -> None:
+    """
+    - Uses the output of the create_image function to save the image or display it
+    - Useless unless the code is being runned in the local environment
+    """
+
+    current_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    if file_save_name is None:
+        save_path = f"done_images/picture_{current_time}.jpg"
+    else:
+        save_path = f"done_images/picture_{file_save_name}_{current_time}.jpg"
+
+    Image.fromarray(image_created).save(save_path)
+    print(f"Image saved at: {save_path}")
+
+
+def full_process(original_image, model, saving_name=None) -> None:
+    """
+    Calls the 3 functions
+    """
+
+    array_original_image = np.array(original_image)
+
+    bbs = predict_bounding_boxes(original_image, model)
+    created_image = create_image(array_original_image, bbs)
+
+    save_image(created_image, saving_name)
+
+    Image.fromarray(created_image)
+
+
+if __name__ == "__main__":
+    model = YOLO("yolov8n.pt")
+    image_file_path = "trial_images/IMG_4194.HEIC"
+
+    register_heif_opener()  # In case if its an iphone picture
+
+    image = Image.open(image_file_path)
+    array_image = np.array(image)
+
+    full_process(image, model, "prueba_now")
