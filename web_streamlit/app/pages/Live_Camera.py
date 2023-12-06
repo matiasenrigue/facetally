e38b30594e30fa5e7f321e0d3d69e9ba3b0fa4f5
Marc Klingenberg <klingenberg.marc20@gmail.com>
2023-12-06 16:25:11 +0100

    stable version

--- web_streamlit/app/pages/Live_Camera.py
+++ web_streamlit/app/pages/Live_Camera.py
@@ -1,33 +1,23 @@
-import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 import av
 import cv2
-import numpy as np
 import requests
-from PIL import Image
 from image_prediction import create_image
 
 
 def callback(frame):
     format = "bgr24"
     img = frame.to_ndarray(format=format)
-    bytes_data = img.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(img, np.uint8), cv2.IMREAD_COLOR)
+
+    _, encoded_image = cv2.imencode(".jpg", img)
+    bytes_data = encoded_image.tobytes()
 
     res = requests.post(
         url="https://face-tally-r5t56frjwa-no.a.run.app/upload_image",
         files={"img": bytes_data},
     ).json()["boundsboxes"]
 
-    array_original_image = np.array(Image.open(img))
-
-    created_image = create_image(
-        array_original_image, res
-    )  # Assuming create_image is defined somewhere
-
-    # st.image(
-    #     Image.fromarray(created_image), caption="You can now save your image"
-    # )  # img = cv2.cvtColor(cv2.Canny(img), cv2.COLOR_GRAY2BGR)
+    created_image = create_image(img, res)  # Assuming create_image is defined somewhere
 
     return av.VideoFrame.from_ndarray(created_image, format=format)
 
