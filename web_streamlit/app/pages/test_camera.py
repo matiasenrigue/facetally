e9e31d78ea6f987620af8db9a7689d7007438d81
Marc Klingenberg <klingenberg.marc20@gmail.com>
2023-12-07 14:13:39 +0100

    wip

---
+++ web_streamlit/app/pages/test_camera.py
@@ -0,0 +1,54 @@
+import av
+import requests
+import streamlit as st
+from image_prediction import create_image
+import cv2
+from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
+
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
+
+
+class VideoProcessor:
+    def __init__(self):
+        self.count = 0
+
+    def recv(self, frame):
+        format = "bgr24"
+        img = frame.to_ndarray(format=format)
+
+        _, encoded_image = cv2.imencode(".jpg", img)
+        bytes_data = encoded_image.tobytes()
+
+        res = requests.post(
+            url="https://face-tally-r5t56frjwa-no.a.run.app/upload_image",
+            files={"img": bytes_data},
+        ).json()["boundsboxes"]
+
+        created_image = create_image(
+            img, res
+        )  # Assuming create_image is defined somewhere
+
+        self.count = len(res)
+
+        return av.VideoFrame.from_ndarray(created_image, format="bgr24")
+
+
+def main():
+    webrtc_ctx = webrtc_streamer(
+        key="WYH",
+        mode=WebRtcMode.SENDRECV,
+        rtc_configuration=RTC_CONFIGURATION,
+        video_processor_factory=VideoProcessor,
+        media_stream_constraints={"video": True, "audio": False},
+        async_processing=False,
+    )
+    if st.button("Count"):
+        count = webrtc_ctx.video_transformer.count
+        st.write(count)
+    # webrtc_ctx
+
+
+if __name__ == "__main__":
+    main()
