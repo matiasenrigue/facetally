c9c51914b6328d425fd5ae884186d41dde664fc1
Marc Klingenberg <klingenberg.marc20@gmail.com>
2023-12-08 10:54:33 +0100

    package versio 1.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requirements = [x.strip() for x in content]  # Save lines from requirements.txt
 
 setup(
     name="facetally",  # Name of the package
-    version="0.0.10",
+    version="1.0.0",
     description="Facetally: package to train and use a face classifier",  # Description of the package
     packages=find_packages(),  # Find packages automatically
     install_requires=requirements,
